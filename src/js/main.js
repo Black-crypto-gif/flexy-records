3f824c5bd2aee5ff007e648c688ebc9e181ce6bb
Your Name <you@youraddress.com>
2022-10-01 16:19:15 +0100

    version2

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,9 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 
 
 
